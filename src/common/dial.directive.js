f063123776a31b80ed47dfa228e3e060957e2fba
William Schmitt <willj@schmittnet.com>
2017-03-26 20:43:16 -0700

    Coverage for dial.

--- src/common/dial.directive.js
+++ src/common/dial.directive.js
@@ -13,7 +13,7 @@
         title: '=',
         value: '=',
       },
-      templateUrl: 'static/html/angular-directives/dial.html',
+      templateUrl: 'static/common/dial.html',
       link: function dialController($scope, $element) {
         // Update the pie chart dial color class based on the value.
         // TODO(will): Get duration to work.
@@ -32,7 +32,7 @@
 
           // Maps a range to color for the dial.
           const colorClasses = {
-            'cc-spc-primary': { min: 0.0, max: 0.0 },
+            'cc-spc-primary': { min: -0.1 /* Captures 0.0. */, max: 0.0 },
             'cc-spc-success': { min: 0.0, max: 0.5 },
             'cc-spc-info': { min: 0.5, max: 0.75 },
             'cc-spc-warning': { min: 0.75, max: 0.90 },
