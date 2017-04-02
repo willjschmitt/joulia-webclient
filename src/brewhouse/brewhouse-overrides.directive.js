8eedb4e983e9229775394fff0f1ad980790d9e2f
William Schmitt <willj@schmittnet.com>
2017-04-01 21:51:10 -0700

    Coverage for overrides.

--- src/brewhouse/brewhouse-overrides.directive.js
+++ src/brewhouse/brewhouse-overrides.directive.js
@@ -9,16 +9,11 @@
     return {
       restrict: 'E',
       transclude: true,
-      scope: {},
-      templateUrl: 'static/brewhouse/brewhouse-overrides.html',
-      link: function brewhouseOverridesController($scope) {
-        // Overridable statuses - sensor ids for the child elements.
-        $scope.heatingElementStatusSensor = 9;
-        $scope.heatingElementStatusSensorOverride = 10;
-
-        $scope.pumpStatusSensor = 11;
-        $scope.pumpStatusSensorOverride = 12;
+      scope: {
+        recipeInstance: '=',
       },
+      templateUrl: 'static/brewhouse/brewhouse-overrides.html',
+      link: function brewhouseOverridesController() {},
     };
   }
 }());
