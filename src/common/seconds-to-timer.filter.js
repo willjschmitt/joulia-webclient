88354b550e111aceea11f4e9603c083d56903b5b
William Schmitt <willj@schmittnet.com>
2017-04-01 19:38:52 -0700

    Adding seconds to timer filter.

---
+++ src/common/seconds-to-timer.filter.js
@@ -0,0 +1,19 @@
+(function loadSecondsToTimerFilter() {
+  angular
+    .module('app.common')
+    .filter('secondsToTimer', secondsToTimerFilter);
+
+  function secondsToTimerFilter() {
+    return function secondsToTimer(secondsAbsolute) {
+      const hours = Math.floor(secondsAbsolute / (60.0 * 60.0));
+      secondsAbsolute -= hours * 60.0 * 60.0;
+      const minutes = Math.floor(secondsAbsolute / 60.0);
+      secondsAbsolute -= minutes * 60.0;
+      const seconds = Math.floor(secondsAbsolute);
+      const hoursString = (hours > 0) ? (`00${hours}:`).slice(-3) : "";
+      const minutesString = (`00${minutes}:`).slice(-3);
+      const secondsString = (`00${seconds}`).slice(-2);
+      return `${hoursString}${minutesString}${secondsString}`;
+    };
+  }
+}());
