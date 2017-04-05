e414ff07f8d11895044085c3393915abb11d4275
William Schmitt <willj@schmittnet.com>
2017-04-04 23:16:01 -0700

    Configuration to support coveralls.

--- karma.conf.js
+++ karma.conf.js
@@ -103,6 +103,10 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress', 'coverage'],
 
+    coverageReporter: {
+      type: "lcov",
+      dir: "coverage/"
+    },
 
     // web server port
     port: 9876,
