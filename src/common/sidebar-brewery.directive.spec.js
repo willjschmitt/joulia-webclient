e524551868229fce3c6c0b1ab7346bd12d04c28a
William Schmitt <willj@schmittnet.com>
2017-04-18 21:19:40 -0700

    Refactoring sidebar further.

---
+++ src/common/sidebar-brewery.directive.spec.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+describe('app.common', function () {
+  beforeEach(module('app.common'));
+  beforeEach(module('joulia.templates'));
+
+  var $rootScope, $compile, $httpBackend;
+
+  beforeEach(inject(function($injector) {
+    $rootScope = $injector.get('$rootScope');
+    $compile = $injector.get('$compile');
+    $httpBackend = $injector.get('$httpBackend');
+  }));
+
+  afterEach(function () {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  describe('sidebar-brewery directive', function () {
+    var element, scope;
+    var breweryQuery, brewhouseQuery;
+
+    beforeEach(function () {
+      breweryQuery = $httpBackend.when('GET', 'brewery/api/brewery')
+        .respond([]);
+
+      brewhouseQuery = $httpBackend.when('GET', 'brewery/api/brewhouse')
+        .respond([]);
+    });
+
+    beforeEach(function () {
+      const html = `
+        <sidebar-brewery>
+        </sidebar-brewery>`;
+      scope = $rootScope.$new();
+      element = $compile(html)(scope);
+      $httpBackend.expectGET('brewery/api/brewery');
+      $httpBackend.expectGET('brewery/api/brewhouse');
+      $httpBackend.flush();
+    });
+
+  });
+
+});
