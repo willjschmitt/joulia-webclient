92f5b5493ea9096ba16bf9ce622832f308286e5a
William Schmitt <willj@schmittnet.com>
2017-04-22 21:28:45 -0700

    Collapse sidebar functionality implemented with angular.

--- src/common/joulia-header.directive.js
+++ src/common/joulia-header.directive.js
@@ -3,9 +3,9 @@
     .module('app.common')
     .directive('jouliaHeader', jouliaHeader);
 
-  jouliaHeader.$inject = [];
+  jouliaHeader.$inject = ['$rootScope'];
 
-  function jouliaHeader() {
+  function jouliaHeader($rootScope) {
     return {
       restrict: 'E',
       transclude: true,
@@ -15,10 +15,25 @@
       templateUrl: 'common/joulia-header.tpl.html',
       link: function jouliaHeaderController($scope) {
         $scope.fullName = fullName;
+        $scope.toggleSidebarClass = 'SidebarOpen';
+
+        $scope.toggleSidebar = toggleSidebar;
 
         function fullName(user) {
           return `${user.first_name} ${user.last_name}`;
         }
+
+        function toggleSidebar() {
+          $rootScope.$broadcast('toggleSidebar');
+        }
+
+        $scope.$on('toggleSidebar', function handleToggleSidebar() {
+          if ($scope.toggleSidebarClass === 'SidebarOpen') {
+            $scope.toggleSidebarClass = 'SidebarClose';
+          } else {
+            $scope.toggleSidebarClass = 'SidebarOpen';
+          }
+        });
       },
     };
   }
