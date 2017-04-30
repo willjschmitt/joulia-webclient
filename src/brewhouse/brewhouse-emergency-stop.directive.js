e639110c6628a43715a94da875171805c775b092
William Schmitt <willj@schmittnet.com>
2017-04-29 18:53:39 -0700

    Adding emergency stop button. Also restyled the end session button.

---
+++ src/brewhouse/brewhouse-emergency-stop.directive.js
@@ -0,0 +1,29 @@
+(function loadBrewhouseTerminatorDirective() {
+  angular
+    .module('app.brewhouse')
+    .directive('brewhouseEmergencyStop', brewhouseEmergencyStop);
+
+  brewhouseEmergencyStop.$inject = ['TimeSeriesUpdater'];
+
+  function brewhouseEmergencyStop(TimeSeriesUpdater) {
+    return {
+      restrict: 'E',
+      transclude: true,
+      scope: {
+        recipeInstance: '=',
+      },
+      templateUrl: 'brewhouse/brewhouse-emergency-stop.tpl.html',
+      link: function brewhouseTerminatorController($scope) {
+        $scope.emergencyStopStatus = new TimeSeriesUpdater(
+            $scope.recipeInstance, 'emergency_stop');
+
+        $scope.toggleEmergencyStop = toggleEmergencyStop;
+
+        function toggleEmergencyStop() {
+          // TODO(willjschmitt): Make lights and sirens go off when toggled on.
+          $scope.emergencyStopStatus.set(!$scope.emergencyStopStatus.latest);
+        }
+      },
+    };
+  }
+}());
