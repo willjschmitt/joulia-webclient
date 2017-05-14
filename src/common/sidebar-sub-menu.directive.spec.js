dc9f3541ce2e4d4005b7eba9e3070f52d94813b3
William Schmitt <willj@schmittnet.com>
2017-05-14 11:49:03 -0700

    Refactoring sidebar for breweries. Brehouses toggle on/off.

---
+++ src/common/sidebar-sub-menu.directive.spec.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+describe('app.common', function () {
+  beforeEach(module('app.common'));
+  beforeEach(module('joulia.templates'));
+
+  var $rootScope, $compile, $httpBackend;
+
+  beforeEach(inject(function($injector) {
+    $rootScope = $injector.get('$rootScope');
+    $compile = $injector.get('$compile');
+  }));
+
+  describe('sidebar-sub-menu directive', function () {
+    var element, scope, isolatedScope;
+
+    beforeEach(function () {
+      const html = `
+        <sidebar-sub-menu></sidebar-sub-menu>`;
+      scope = $rootScope.$new();
+      element = $compile(html)(scope);
+      scope.user = {};
+      scope.$digest();
+      isolatedScope = element.isolateScope();
+    });
+
+    it('compiles html', function () {
+      expect(element.html()).toContain('submenu-inner-wrapper');
+    });
+
+  });
+
+});
