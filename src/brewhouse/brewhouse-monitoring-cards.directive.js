f717e7f721f8772fed2cc97f2fcfe5a8f8f046a8
William Schmitt <willj@schmittnet.com>
2017-06-16 09:56:21 -0700

    Changing to calculation of energy cost on the frontend.

--- src/brewhouse/brewhouse-monitoring-cards.directive.js
+++ src/brewhouse/brewhouse-monitoring-cards.directive.js
@@ -3,9 +3,9 @@
     .module('app.brewhouse')
     .directive('brewhouseMonitoringCards', brewhouseMonitoringCards);
 
-  brewhouseMonitoringCards.$inject = [];
+  brewhouseMonitoringCards.$inject = ['userService'];
 
-  function brewhouseMonitoringCards() {
+  function brewhouseMonitoringCards(userService) {
     return {
       restrict: 'E',
       transclude: true,
@@ -13,7 +13,22 @@
         recipeInstance: '=',
       },
       templateUrl: 'brewhouse/brewhouse-monitoring-cards.tpl.html',
-      link: function brewhouseMonitoringCardsController() {},
+      link: function brewhouseMonitoringCardsController($scope) {
+        $scope.energyUsageToEnergyCost = energyUsageToEnergyCost;
+
+        /**
+         * Uses the user preference for energy cost rate (in $/kWh), and
+         * converts an input of energy (in Wh) into $.
+         *
+         * @param{Number} energyUsage Energy consumed (in Wh).
+         * @returns{Number} Cost of the energy consumed (in $).
+         */
+        function energyUsageToEnergyCost(energyUsage) {
+          const kWattHoursPerKilowattHour = 1000.0;
+          const energyUsageKWh = energyUsage / kWattHoursPerKilowattHour;
+          return energyUsageKWh * userService.userPreferences.energy_cost_rate;
+        }
+      },
     };
   }
 }());
