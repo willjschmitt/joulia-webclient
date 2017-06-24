12a782c168a20e50ba8c32d92a29ebd82f8fc062
William Schmitt <willj@schmittnet.com>
2017-06-24 13:54:38 -0700

    Adding screenshots.

--- src/public/public-screenshots.directive.js
+++ src/public/public-screenshots.directive.js
@@ -11,7 +11,20 @@
       transclude: true,
       scope: {},
       templateUrl: 'public/public-screenshots.tpl.html',
-      link: function publicScreenshotsController() {},
+      link: function publicScreenshotsController() {
+        $('#owl-screenshot').owlCarousel({
+          // 10 items above 1000px browser width.
+          items: 4,
+          // 5 items between 1120px and 941px.
+          itemsDesktop: [1120, 3],
+          // Between 940px and 601px.
+          itemsDesktopSmall: [940, 2],
+          // 2 items between 600 and 0.
+          itemsTablet: [600, 2],
+          // itemsMobile disabled - inherit from itemsTablet option.
+          itemsMobile: [400, 1],
+        });
+      },
     };
   }
 }());
