c8ad41e510f237ad00cfe25d1f319a8326239e39
William Schmitt <william@wschmitt.com>
2017-08-29 20:34:34 -0700

    Supports ingredient additions.

--- src/recipes/ingredient-addition.directive.js
+++ src/recipes/ingredient-addition.directive.js
@@ -11,7 +11,7 @@
       transclude: true,
       scope: {
         // The IngredientAddition to edit.
-        ingredientAddition: '=',
+        ingredientAddition: '=addition',
 
         // The ingredients to select from for the addition.
         ingredients: '=',
@@ -20,16 +20,17 @@
         // removed, it is removed from the parent array.
         // TODO(willjschmitt): Reconsider passing the array, and rather
         // broadcast an event.
-        ingredientAdditions: '=',
+        ingredientAdditions: '=additions',
       },
-      templateUrl: 'recipes/ingredient-additions.tpl.html',
+      templateUrl: 'recipes/ingredient-addition.tpl.html',
       link: function ingredientAdditionsController($scope) {
         $scope.updateIngredientAddition = updateIngredientAddition;
         $scope.removeIngredientAddition = removeIngredientAddition;
         $scope.steps = breweryResources.BREWING_STEP_CHOICES_ordered;
         $scope.units = breweryResources.UNITS_CHOICES_ordered;
 
-        $scope.amountUserUnits = 0.0;
+        $scope.userAmount = 0.0;
+        $scope.unitsToGrams = 0.0;
 
         /**
          * Updates provided resource.
@@ -48,13 +49,13 @@
               () => $scope.ingredientAdditions.splice(index, 1));
         }
 
-        /* function unitsToGramsConversionFactor() {
-          $scope.amountUserUnitsToIngredientAdditionAmount
+        function calculateUnitsToGrams() {
+          $scope.unitsToGrams
               = breweryResources.UNITS_CHOICES_conversion_factors[
                   $scope.ingredientAddition.units];
-        }*/
+        }
 
-        // $scope.$watch('ingredientAddition.units', unitsToGramsConversionFactor);
+        $scope.$watch('ingredientAddition.units', calculateUnitsToGrams);
 
         /**
          * A $watchGroup function to be executing when two mutually, but
@@ -64,44 +65,74 @@
          * values[1] = values[0] * values[2]
          * y = k * x
          */
-        function updateMutuallyCoupledVariables(newValues, oldValues, scope) {
-          /* const newAmountUserUnits = newValues[0];
-          const newIngredientAdditionAmount = newValues[1];
-          const oldAmountUserUnits = oldValues[0];
-          const oldIngredientAdditionAmount = oldValues[1];
+        function updateMutuallyCoupledVariables(newValues, oldValues) {
+          const newUserAmount = newValues[0];
+          const oldUserAmount = oldValues[0];
+          const newAmount = newValues[1];
+          const oldAmount = oldValues[1];
+          const newUnitsToGrams = newValues[2];
+          const oldUnitsToGrams = oldValues[2];
 
-          const amountUserUnitsToIngredientAdditionAmount = newValues[2];
+          const changedAmount = newAmount !== oldAmount;
+          const changedUserAmount = newUserAmount !== oldUserAmount;
+          const changedUnitsToGrams = newUnitsToGrams !== oldUnitsToGrams;
 
-          let altAmountUserUnits = newAmountUserUnits;
-          let altIngredientAdditionAmount = newIngredientAdditionAmount;
-          if (newAmountUserUnits !== oldAmountUserUnits) {
-            altIngredientAdditionAmount
-                = newAmountUserUnits
-                    / amountUserUnitsToIngredientAdditionAmount;
-          }
-          if (newIngredientAdditionAmount !== oldIngredientAdditionAmount) {
-            altAmountUserUnits
-                = newIngredientAdditionAmount
-                    * amountUserUnitsToIngredientAdditionAmount;
+          // This happens when the dependent variable is changed, and the change
+          // from the first variable is still detected.
+          if (changedAmount + changedUserAmount + changedUnitsToGrams > 1) {
+            throw new Error(''.concat(
+                'More than one watched variable changed, which should not',
+                ` happen. changedUserAmount: ${changedUserAmount}; `,
+                ` changedAmount: ${changedAmount}; changedUnitsToGrams: `,
+                ` ${changedUnitsToGrams}.`));
           }
 
-          if (altIngredientAdditionAmount !== newIngredientAdditionAmount) {
-            console.log('a');
+          let altAmount = newAmount;
+          let altUserAmount = newUserAmount;
+          if (changedUnitsToGrams) {
+            // It's assumed changes to units should drive backend amounts rather
+            // than changing the user displayed amount.
+            altAmount = newUserAmount / newUnitsToGrams;
+          } else if (changedUserAmount) {
+            // If the user displayed value is changed (by the user), the backend
+            // value also should be changed.
+            altAmount = newUserAmount / newUnitsToGrams;
+          } else if (changedAmount) {
+            // If the backend amount changed (presumably by getting the
+            // original value from the backend), the user displayed value should
+            // be displayed.
+            altUserAmount = newAmount * newUnitsToGrams;
+          } else {
+            // If nothing has changed, the initial values have been recieved,
+            // and we should attempt the setting of the user-displayed value
+            // right away.
+            altUserAmount = newAmount * newUnitsToGrams;
           }
-          if (altAmountUserUnits !== newAmountUserUnits) {
-            console.log('b');
-          }*/
-          console.log(scope);
+
+          // These checks now only change the variable not changed by the
+          // $watchCollection. This avoids triggering infinite looping from
+          // continuously changing the watched variables in an alternating
+          // fashion. We use approximate float matching against smallError,
+          // since these are calculated results.
+          // TODO(willjschmitt): Try to figure out a way to generalize the
+          // updating of the non-changed variable. This is impossible without
+          // a generic way of getting a reference to the changed variables.
+          const kSmallError = 1E-6;
+          if (Math.abs(altAmount - newAmount) > kSmallError) {
+            $scope.ingredientAddition.amount = altAmount;
+            updateIngredientAddition();
+          }
+          if (Math.abs(altUserAmount - newUserAmount) > kSmallError) {
+            $scope.userAmount = altUserAmount;
+          }
         }
 
         /**
          * Updates weight value after a change with the user version into grams.
          */
-        $scope.$watchGroup(
-          [
-            'amountUserUnits', 'ingredientAddition.amount',
-            'amountUserUnitsToIngredientAdditionAmount',
-          ], updateMutuallyCoupledVariables);
+        $scope.$watchCollection(
+            '[userAmount, ingredientAddition.amount, unitsToGrams]',
+            updateMutuallyCoupledVariables);
       },
     };
   }
