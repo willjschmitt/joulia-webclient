70d04ce0508d4f107a374b420177c414388016a8
William Schmitt <william@wschmitt.com>
2017-09-21 22:06:25 -0700

    Adds recipe retrospective page.

--- src/recipes/recipes.module.js
+++ src/recipes/recipes.module.js
@@ -17,6 +17,11 @@
         templateUrl: 'recipes/recipes.tpl.html',
         controller: 'RecipesController',
         controllerAs: 'recipesCtrl',
+      })
+      .when('/recipeInstance/:recipeInstanceId', {
+        templateUrl: 'recipes/recipe-instance-retrospective.controller.tpl.html',
+        controller: 'RecipeInstanceRetrospectiveController',
+        controllerAs: 'recipeInstanceRetrospectiveCtrl',
       });
   }
 }());
