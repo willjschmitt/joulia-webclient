f40d0800b2dae35078797e0a10403b5e932268df
William Schmitt <william@wschmitt.com>
2017-09-21 22:51:36 -0700

    Adds past sessions to inactive brewhouse page.

--- src/brewhouse/brewhouse-inactive.directive.spec.js
+++ src/brewhouse/brewhouse-inactive.directive.spec.js
@@ -3,20 +3,41 @@ describe('app.brewhouse', function () {
   beforeEach(module('app.brewhouse'));
   beforeEach(module('joulia.templates'));
 
-  var $rootScope, $compile, $interval;
+  var $rootScope, $compile, $interval, $httpBackend;
 
   beforeEach(inject(function($injector) {
     $rootScope = $injector.get('$rootScope');
     $compile = $injector.get('$compile');
     $interval = $injector.get('$interval');
+    $httpBackend = $injector.get('$httpBackend');
   }));
 
   describe('brewhouse-terminator directive', function () {
     var element, scope, isolatedScope;
-    const html = '<brewhouse-inactive></brewhouse-inactive>';
+    var recipeInstanceQuery, recipeQuery;
+    const html = `
+      <brewhouse-inactive
+          brewhouse="brewhouse">
+      </brewhouse-inactive>`;
+
+    beforeEach(function () {
+      recipeInstanceQuery = $httpBackend.whenGET(
+          'brewery/api/recipeInstance?brewhouse=5')
+        .respond([]);
+
+      recipeQuery = $httpBackend.whenGET('brewery/api/recipe')
+        .respond([]);
+    });
 
     beforeEach(function () {
       scope = $rootScope.$new();
+      scope.brewhouse = {
+        id: 5,
+      };
+
+      $httpBackend.expectGET('brewery/api/recipeInstance?brewhouse=5');
+      $httpBackend.expectGET('brewery/api/recipe');
+
       element = $compile(html)(scope);
       scope.$digest();
       isolatedScope = element.isolateScope();
