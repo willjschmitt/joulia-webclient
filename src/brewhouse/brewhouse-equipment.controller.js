70ce25128156474ecf6403f5462131b8fbe53e7a
William Schmitt <willj@schmittnet.com>
2017-11-19 13:39:05 -0800

    Hoses are not animated during prep states.

--- src/brewhouse/brewhouse-equipment.controller.js
+++ src/brewhouse/brewhouse-equipment.controller.js
@@ -16,49 +16,81 @@
     $scope.hoseStatus = {};
     $scope.$watch('currentStatus.latest', function updateHoseStatus() {
       switch ($scope.currentStatus.latest) {
-      case states.statesEnum.statePremash:
-      case states.statesEnum.stateStrike:
+      case states.statesEnum.Prestart:
+        $scope.hoseStatus = {};
+        break;
+      case states.statesEnum.Premash:
         $scope.hoseStatus = {
-          hltOutToPumpIn: true,
-          pumpOutToMashTunIn: true,
+          hltOutToPumpIn: {connected: true},
+          pumpOutToMashTunIn: {connected: true},
+        };
+        break;
+      case states.statesEnum.Strike:
+        $scope.hoseStatus = {
+          hltOutToPumpIn: {on: true},
+          pumpOutToMashTunIn: {on: true},
         };
         break;
       case states.statesEnum.PostStrike:
+        $scope.hoseStatus = {
+          pumpOutToHltExchangerIn: {connected: true},
+          hltExchangerOutToMashTunIn: {connected: true},
+          mashTunOutToPumpIn: {connected: true},
+        };
+        break;
       case states.statesEnum.Mash:
       case states.statesEnum.MashoutRamp:
       case states.statesEnum.MashoutRecirculation:
         $scope.hoseStatus = {
-          pumpOutToHltExchangerIn: true,
-          hltExchangerOutToMashTunIn: true,
-          mashTunOutToPumpIn: true,
+          pumpOutToHltExchangerIn: {on: true},
+          hltExchangerOutToMashTunIn: {on: true},
+          mashTunOutToPumpIn: {on: true},
         };
         break;
       case states.statesEnum.SpargePrep:
+        $scope.hoseStatus = {
+          hltOutToPumpIn: {connected: true},
+          pumpOutToMashTunIn: {connected: true},
+          mashTunOutToGround: {connected: true},
+        };
+        break;
       case states.statesEnum.Sparge:
         $scope.hoseStatus = {
-          hltOutToPumpIn: true,
-          pumpOutToMashTunIn: true,
-          mashTunOutToGround: true,
+          hltOutToPumpIn: {on: true},
+          pumpOutToMashTunIn: {on: true},
+          mashTunOutToGround: {on: true},
         };
         break;
       case states.statesEnum.PreBoil:
+        $scope.hoseStatus = {
+          mashTunOutToPumpIn: {connected: true},
+          pumpOutToHltIn: {connected: true},
+        };
+        break;
       case states.statesEnum.MashToBoil:
         $scope.hoseStatus = {
-          mashTunOutToPumpIn: true,
-          pumpOutToHltIn: true,
+          mashTunOutToPumpIn: {on: true},
+          pumpOutToHltIn: {on: true},
         };
+        break;
+      case states.statesEnum.BoilPreheat:
+      case states.statesEnum.Boil:
+        $scope.hoseStatus = {};
         break;
       case states.statesEnum.Cool:
         $scope.hoseStatus = {
-          waterInToPumpIn: true,
-          pumpOutToHltExchangerIn: true,
-          hltExchangerOutToGround: true,
+          waterInToPumpIn: {on: true},
+          pumpOutToHltExchangerIn: {on: true},
+          hltExchangerOutToGround: {on: true},
         };
         break;
       case states.statesEnum.PumpOut:
         $scope.hoseStatus = {
-          hltOutToGround: true,
+          hltOutToGround: {on: true},
         };
+        break;
+      case states.statesEnum.Done:
+        $scope.hoseStatus = {};
         break;
       default:
         $scope.hoseStatus = {};
