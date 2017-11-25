d339ab10e535fe053d1815e99dbe89202f4d3701
William Schmitt <willj@schmittnet.com>
2017-11-25 14:48:01 -0500

    Fix linting errors

--- src/brewhouse/brewhouse-equipment.controller.js
+++ src/brewhouse/brewhouse-equipment.controller.js
@@ -21,56 +21,56 @@
         break;
       case states.statesEnum.Premash:
         $scope.hoseStatus = {
-          hltOutToPumpIn: {connected: true},
-          pumpOutToMashTunIn: {connected: true},
+          hltOutToPumpIn: { connected: true },
+          pumpOutToMashTunIn: { connected: true },
         };
         break;
       case states.statesEnum.Strike:
         $scope.hoseStatus = {
-          hltOutToPumpIn: {on: true},
-          pumpOutToMashTunIn: {on: true},
+          hltOutToPumpIn: { on: true },
+          pumpOutToMashTunIn: { on: true },
         };
         break;
       case states.statesEnum.PostStrike:
         $scope.hoseStatus = {
-          pumpOutToHltExchangerIn: {connected: true},
-          hltExchangerOutToMashTunIn: {connected: true},
-          mashTunOutToPumpIn: {connected: true},
+          pumpOutToHltExchangerIn: { connected: true },
+          hltExchangerOutToMashTunIn: { connected: true },
+          mashTunOutToPumpIn: { connected: true },
         };
         break;
       case states.statesEnum.Mash:
       case states.statesEnum.MashoutRamp:
       case states.statesEnum.MashoutRecirculation:
         $scope.hoseStatus = {
-          pumpOutToHltExchangerIn: {on: true},
-          hltExchangerOutToMashTunIn: {on: true},
-          mashTunOutToPumpIn: {on: true},
+          pumpOutToHltExchangerIn: { on: true },
+          hltExchangerOutToMashTunIn: { on: true },
+          mashTunOutToPumpIn: { on: true },
         };
         break;
       case states.statesEnum.SpargePrep:
         $scope.hoseStatus = {
-          hltOutToPumpIn: {connected: true},
-          pumpOutToMashTunIn: {connected: true},
-          mashTunOutToGround: {connected: true},
+          hltOutToPumpIn: { connected: true },
+          pumpOutToMashTunIn: { connected: true },
+          mashTunOutToGround: { connected: true },
         };
         break;
       case states.statesEnum.Sparge:
         $scope.hoseStatus = {
-          hltOutToPumpIn: {on: true},
-          pumpOutToMashTunIn: {on: true},
-          mashTunOutToGround: {on: true},
+          hltOutToPumpIn: { on: true },
+          pumpOutToMashTunIn: { on: true },
+          mashTunOutToGround: { on: true },
         };
         break;
       case states.statesEnum.PreBoil:
         $scope.hoseStatus = {
-          mashTunOutToPumpIn: {connected: true},
-          pumpOutToHltIn: {connected: true},
+          mashTunOutToPumpIn: { connected: true },
+          pumpOutToHltIn: { connected: true },
         };
         break;
       case states.statesEnum.MashToBoil:
         $scope.hoseStatus = {
-          mashTunOutToPumpIn: {on: true},
-          pumpOutToHltIn: {on: true},
+          mashTunOutToPumpIn: { on: true },
+          pumpOutToHltIn: { on: true },
         };
         break;
       case states.statesEnum.BoilPreheat:
@@ -79,14 +79,14 @@
         break;
       case states.statesEnum.Cool:
         $scope.hoseStatus = {
-          waterInToPumpIn: {on: true},
-          pumpOutToHltExchangerIn: {on: true},
-          hltExchangerOutToGround: {on: true},
+          waterInToPumpIn: { on: true },
+          pumpOutToHltExchangerIn: { on: true },
+          hltExchangerOutToGround: { on: true },
         };
         break;
       case states.statesEnum.PumpOut:
         $scope.hoseStatus = {
-          hltOutToGround: {on: true},
+          hltOutToGround: { on: true },
         };
         break;
       case states.statesEnum.Done:
