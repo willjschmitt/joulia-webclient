e73082e47c56e3414768cf1355159cfeeca85111
William Schmitt <william@wschmitt.com>
2018-01-06 17:52:10 -0800

    Add delete modal.

---
+++ src/common/confirm-delete.controller.spec.js
@@ -0,0 +1,48 @@
+/* eslint-disable */
+describe('app.common confirm-delete.controller', function () {
+  beforeEach(module('app.common'));
+  beforeEach(module('joulia.templates'));
+
+  var $controller, $rootScope;
+
+  beforeEach(inject(function ($injector) {
+    $controller = $injector.get('$controller');
+    $rootScope = $injector.get('$rootScope');
+  }));
+
+  describe('ConfirmDeleteController', function () {
+    var controller, scope;
+    var brewingCompanyQuery, brewerySave;
+    var $uibModalInstance;
+
+    beforeEach(function () {
+      $uibModalInstance = jasmine.createSpyObj(
+        '$uibModalInstance', ['close', 'dismiss']);
+
+      scope = $rootScope.$new();
+      controller = $controller('ConfirmDeleteController', {
+        $scope: scope,
+        $uibModalInstance: $uibModalInstance,
+        name: "foo",
+      });
+    });
+
+    it('should be created successfully', function () {
+      expect(controller).toBeDefined();
+    });
+
+    describe('ok', function () {
+      it('should dismiss with close', function () {
+        scope.ok();
+        expect($uibModalInstance.close).toHaveBeenCalled();
+      });
+    });
+
+    describe('cancel', function () {
+      it('should dismiss modal', function () {
+        scope.cancel();
+        expect($uibModalInstance.dismiss).toHaveBeenCalled();
+      });
+    });
+  });
+});
