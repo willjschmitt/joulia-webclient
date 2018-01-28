7e50087c3825a2e4b9c01166fb7946dcdb06966f
William Schmitt <william@wschmitt.com>
2018-01-28 13:26:30 -0800

    Add yeast selection for fermentation to recipe.

---
+++ src/recipes/recipe-fermentation.directive.js
@@ -0,0 +1,66 @@
+(function loadRecipeFermentationDirective() {
+  angular
+    .module('app.recipes')
+    .directive('recipeFermentation', recipeFermentation);
+
+  recipeFermentation.$inject = ['breweryResources'];
+
+  function recipeFermentation(breweryResources) {
+    return {
+      restrict: 'E',
+      transclude: true,
+      scope: {
+        // The recipe the ingredient addition is attached to, so it can be
+        // updated when ingredient additions are updated.
+        recipe: '=recipe',
+      },
+      templateUrl: 'recipes/recipe-fermentation.tpl.html',
+      link: function recipeFermentationController($scope, $element) {
+        $scope.yeastResource = breweryResources.YeastIngredientSearch;
+        $scope.updateRecipe = updateRecipe;
+        $scope.yeastIngredientHTML = `
+          <div>{{ name }}</div>
+          <div>
+            <small>
+              Attenuation: {{ low_attenuation * 100 | number : 0 }}-{{ high_attenuation * 100 | number : 0 }}%
+              <br/>
+              ABV Tolerance: {{ low_abv_tolerance * 100 | number : 1 }}-{{ high_abv_tolerance * 100 | number : 0 }}%
+              <br/>
+              Temperature: {{ low_temperature }}-{{ high_temperature }}&deg;F
+            </small>
+          </div>`;
+
+        loadingElement().circularProgress();
+
+        /**
+         * Updates provided recipe.
+         */
+        function updateRecipe() {
+          showLoadingElement();
+          $scope.recipe.$update(hideLoadingElement, hideLoadingElement);
+        }
+
+        /**
+         * Retrieves the loading element in this panel.
+         */
+        function loadingElement() {
+          return angular.element($element).find('#yeast-loading');
+        }
+
+        /**
+         * Shows the loading icon. That is, starts the icon spinning.
+         */
+        function showLoadingElement() {
+          loadingElement().circularProgress('show');
+        }
+
+        /**
+         * Hides the loading icon. That is, stops the icon spinning.
+         */
+        function hideLoadingElement() {
+          loadingElement().circularProgress('hide');
+        }
+      },
+    };
+  }
+}());
