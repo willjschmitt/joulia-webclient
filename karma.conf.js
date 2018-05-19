fe95bd1333e0fb8534ae02db9f056e908aa26e82
William Schmitt <william@wschmitt.com>
2018-05-19 08:10:44 -0700

    Cleanup karma config.

--- karma.conf.js
+++ karma.conf.js
@@ -16,27 +16,9 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // Third-party libraries loaded by bower.
-      'node_modules/jquery/dist/jquery.js',
-      // 'node_modules/jquery-ui-dist/jquery-ui.js',
-      // 'node_modules/angular/angular.js',
-      // 'node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
-      // 'node_modules/ui-select/dist/select.min.js',
-      // 'node_modules/angular-mocks/angular-mocks.js',
-      // 'node_modules/angular-resource/angular-resource.js',
-      // 'node_modules/angular-sanitize/angular-sanitize.js',
-      // 'node_modules/@uirouter/angularjs/release/angular-ui-router.js',
-      //'node_modules/angular-websocket/dist/angular-websocket.js',
-      //'node_modules/angular-websocket/dist/angular-websocket-mock.js',
-      // 'node_modules/perfect-scrollbar/dist/perfect-scrollbar.js',
-      // 'node_modules/ui-select/dist/select.js',
-      // 'node_modules/d3/d3.js',
-      // 'node_modules/nvd3/build/nv.d3.js',
-      // 'node_modules/underscore/underscore.js',
-      // 'node_modules/moment/moment.js',
-      // 'node_modules/screenfull/dist/screenfull.js',
-
       // TODO(willjschmitt): Remove these when figuring out how to import them
       // correctly.
+      'node_modules/jquery/dist/jquery.js',
       'node_modules/flexslider/jquery.flexslider-min.js',
       'node_modules/owl.carousel/dist/owl.carousel.js',
 
@@ -57,24 +39,6 @@ module.exports = function(config) {
       'vendor/simplePieChart/jquery.simplePieChart.js',
       'vendor/peity/jquery.peity.min.js',
 
-      // Third-party proprietary licensed libaries.
-      //'vendor/bemat-admin/js/bemat-admin-common.js',
-
-      // Load in test helper functions.
-      'temp/js/test/input-checkers.js',
-
-      // Need to manually load the modules for now to make sure they are
-      // available for when adding the controllers, etc., to them.
-      // TODO(will): Figure out a better way to not have to manually include
-      // the module definitions.
-      'temp/js/joulia-webclient.js',
-      'temp/js/common/common.module.js',
-      'temp/js/public/public.module.js',
-      'temp/js/dashboard/dashboard.module.js',
-      'temp/js/recipes/recipes.module.js',
-      'temp/js/brewhouse/brewhouse.module.js',
-      'temp/js/profile/profile.module.js',
-
       // The main source files.
       'temp/**/*.js',
     ],
@@ -83,13 +47,8 @@ module.exports = function(config) {
     // list of files to exclude
     exclude: [],
 
-
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    // preprocessors: {
-    //   'dist/**/*.js': ['coverage'],
-    // },
-
     preprocessors: {
       'temp/**/*.js': [ 'browserify', 'coverage' ]
     },
